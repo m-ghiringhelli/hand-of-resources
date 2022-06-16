07ef11ae1eba4cfe6b5307fe593601680b68af03
m-ghiringhelli <mghiring@gmail.com>
2022-06-16 12:56:06 -0700

    seeded venues

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS venues;
+
+CREATE TABLE venues (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  quadrant VARCHAR NOT NULL,
+  capacity INT NOT NULL
+);
+
+INSERT INTO venues (name, quadrant, capacity) VALUES
+  ('Wonder Ballroom', 'NE', 800), 
+  ('Doug Fir', 'SE', 300),
+  ('Crystal Ballroom', 'SW', 1500);
