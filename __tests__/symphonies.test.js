7019699f5e06efb45ba7f8482f8918bb5a8dc53e
m-ghiringhelli <mghiring@gmail.com>
2022-06-17 14:54:03 -0700

    successfully fetches all symphonies

--- __tests__/symphonies.test.js
+++ __tests__/symphonies.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { Symphony } = require('../lib/models/Symphony');
 
 describe('symphony routes', () => {
   beforeEach(() => {
