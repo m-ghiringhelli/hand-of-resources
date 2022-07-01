bfc7541e5117fe9c83fb70a2d32fb3fc665dfaba
m-ghiringhelli <mghiring@gmail.com>
2022-07-01 11:17:24 -0700

    successfully fetches all beatles

--- __tests__/beatles.test.js
+++ __tests__/beatles.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('beateles routes', () => {
+describe('beatles routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
